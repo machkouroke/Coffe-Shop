71f94989ea0af31fff78b56f1210f754be0ad556
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-08-12 17:14:43 +0100

    Add of env variable

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,14 @@
-/* @TODO replace with your variables
+/*
  * ensure all variables on this page match your project
  */
 
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
-  auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    production: false,
+    apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+    auth0: {
+        url: 'lopuniverse.us', // the auth0 domain prefix
+        audience: 'image', // the audience set for the auth0 app
+        clientId: 'JuCSr0oEc9Mx91wXZRUIIqb1a9vf5DwV', // the client id generated for the auth0 app
+        callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+    }
 };
